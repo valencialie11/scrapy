32f1762966d8b124ab1e56bbc0059cbede4be651
Valencia Lie <valencialie11@yahoo.com>
2020-06-23 06:46:36 +0700

    still cannot crawl to next page

--- scraper.py
+++ scraper.py
@@ -17,18 +17,15 @@ class YAnovels(scrapy.Spider):
                 'Name': YA_novels.xpath(NAME_SELECTOR).extract_first(),
                 'Author': YA_novels.xpath(AUTHOR_SELECTOR).extract_first(),
                 'Ratings': YA_novels.xpath(RATINGS_SELECTOR).extract_first(),
-                '': YA_novels.xpath(SCORE_SELECTOR).extract_first(),
+                'Score': YA_novels.xpath(SCORE_SELECTOR).extract_first(),
                 'Number of people who voted': YA_novels.xpath(VOTES_SELECTOR).extract_first(),
                 'Image': YA_novels.css(IMAGE_SELECTOR).extract_first()
                 #we just want the first element that matches the selector. This gives us a string, rather than a list of elements.
             }
         
-        NEXTPAGE_SELECTOR = './/a[contains(@class, "next_page")]/a[contains(@rel, "next")]/a[@href]'
-        next_page = response.xpath(NEXTPAGE_SELECTOR).extract_first()
+        NEXT_PAGE_SELECTOR = './/a[contains(@class, "next_page")]/a[contains(@rel, "next")]/a[@href]'
+        next_page = response.xpath(NEXT_PAGE_SELECTOR).extract_first()
         if next_page:
-            yield scrapy.Request(
-                response.urljoin(next_page),
-                callback=self.parse
-            )
-            #The scrapy.Request is a value that we return saying “Hey, crawl this page”
-            #callback=self.parse says “once you’ve gotten the HTML from this page, pass it back to this method so we can parse it, extract the data, and find the next page.“
+            next_page_url = 'https://www.goodreads.com' + next_page
+            request = scrapy.Request(url=next_page_url)
+            yield request
